20dba27ddf136e6f0420dc8379bdaa4e37baa21c
Sadia Thasina Samanta <sadiakvs.724@gmail.com>
2023-07-26 10:42:17 +0400

    Initial Commit

---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by Sadia Thasina on 26/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
